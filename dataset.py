c327e18329c1b5361d343dfe0583b6e125e63418
Trygve Nerland <trygve55@gmail.com>
2020-02-19 12:46:50 +0100

    Update scraper

--- dataset.py
+++ dataset.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import pandas as pd
 import time
 from finn import _interpolate_data_, _data_cleaner
+import numpy as np
 
 if __name__ == '__main__':
     codes = finncode.scrape_category("https://www.finn.no/realestate/homes/search.html?geoLocationName=Kristiansund%2C+M%C3%B8re+og+Romsdal&lat=63.11045&lon=7.72795&radius=1500")
@@ -26,9 +27,17 @@ if __name__ == '__main__':
                 time.sleep(2)
 
     df = pd.DataFrame(ads)
+
+    #soverom
     df['Soverom'] = pd.to_numeric(df['Soverom'])
     df = df[df['Soverom'].notna()]
-    df = df.dropna(axis=1)
+
+    #pris
+    df['Totalpris'].replace('  ', np.nan, inplace=True)
+    df = df.dropna(subset=['Totalpris'])
+
+    #rare
+    #df = df.dropna(axis=1)
     df.to_csv(r'test.csv', index=False)
 
     print("Saved CSV file to test.csv")
