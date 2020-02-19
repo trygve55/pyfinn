3206e4072e528413dfaaa17e022d965fdd9adda9
Trygve Nerland <trygve55@gmail.com>
2020-02-19 11:28:28 +0100

    Fix timeout

--- dataset.py
+++ dataset.py
@@ -9,19 +9,21 @@ if __name__ == '__main__':
     codes = finncode.scrape_category("https://www.finn.no/realestate/homes/search.html?geoLocationName=Kristiansund%2C+M%C3%B8re+og+Romsdal&lat=63.11045&lon=7.72795&radius=1500")
     ads = []
     for code in tqdm(codes):
+        tries_left = 3
         ad_data = None
-        while True:
+        while tries_left:
             try:
+                tries_left -= 1
                 ad_data = finn.scrape_ad(code)
                 ad_data = _interpolate_data_(ad_data)
                 ad_data = _data_cleaner(ad_data)
+
+                if 'Byggeår' in ad_data:
+                    ads.append(ad_data)
                 break
             except:
                 print('error on', code)
                 time.sleep(2)
-
-        if 'Byggeår' in ad_data:
-            ads.append(ad_data)
 
     df = pd.DataFrame(ads)
     df['Soverom'] = pd.to_numeric(df['Soverom'])
